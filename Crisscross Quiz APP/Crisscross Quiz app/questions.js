0e5c14660f3a6522571f96f0c383b9ec0927b980
crisscross482 <108285358+crisscross482@users.noreply.github.com>
2023-04-14 02:00:27 +0000

    Add files via upload

---
+++ Crisscross Quiz APP/Crisscross Quiz app/questions.js
@@ -0,0 +1,114 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which team won the champions league for 2017 - 2018 seasion?",
+    answer: "Real Madrid FC",
+    options: [
+      "Barcelona FC",
+      "Bayern Munchen FC",
+      "Real Madrid FC",
+      "Paris Saint German FC"
+    ]
+  },
+    {
+      numb: 4,
+      question: "Which of the following is not a physical threat?",
+      answer: "Digital threat",
+      options: [
+        "Hardware threat",
+        "Electrical threat",
+        "Digital threat",
+        "Environmental threat"
+      ]
+  },
+    {
+    numb: 4,
+    question: "Which African country first gain independence?",
+    answer: "Ghana",
+    options: [
+      "Liberia",
+      "Egypt",
+      "Ghana",
+      "South Africa"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  
+    {
+    numb: 6,
+    question: "Javascript is an _________ language?",
+    answer: "Object-Oriented",
+    options: [
+      "Object-Oriented",
+      "Object-Based",
+      "Procedural",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 7,
+    question: "The following are types of vulnerabilities except?",
+    answer: "Hardware vulnerabilities",
+    options: [
+      "Technological Vulnerabilities",
+      "Hardware vulnerabilities",
+      "Configuration Vulnerabilities",
+      "Security Policies Vulnerabilities"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Which of the following is not a componebt of a server?",
+    answer: "Data",
+    options: [
+      "Security",
+      "Software",
+      "Data",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Which of the following is not a Javascript framework?",
+    answer: "Cassandra",
+    options: [
+      "Node",
+      "Vue",
+      "React",
+      "Cassandra"
+    ]   
+  },
+  {
+    numb: 10,
+    question: "Which of the following are closures in Javascript?",
+    answer: "Functions",
+    options: [
+      "Variables",
+      "Functions",
+      "Objects ",
+      "All of the above"
+    ]
+  }
+];
